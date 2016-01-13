71ae42de9d0caf042c3dd0e7b0a845e2758e5c20
Dan Hennion <carbide20@gmail.com>
2016-01-12 19:46:04 -0500

    Code cleanup

--- public/js/pollForm.js
+++ public/js/pollForm.js
@@ -2,8 +2,6 @@
 // Event listener for the 'Add Question' button
 document.getElementById("addQuestion").addEventListener("click", function() {
 
-	console.log('adding question!');
-
 	// Get all question items
 	var questionElement = document.getElementsByClassName("question");
 
@@ -13,11 +11,10 @@ document.getElementById("addQuestion").addEventListener("click", function() {
 	// Loop through the questions
 	for (var i = 0; i < questionElement.length; i++) {
 
-		// Add listeners to the add answer buttons
+		// TODO: Add listeners to the add answer buttons
 		//addAnswerElement[i].addEventListener('click', addAnswer, false);
 
 		lastQuestion = questionElement[i].name;
-		console.log(lastQuestion);
 
 	}
 
@@ -25,6 +22,15 @@ document.getElementById("addQuestion").addEventListener("click", function() {
 	lastQuestion = lastQuestion.split('question');
 	lastQuestion = parseInt(lastQuestion[1]);
 
+
+	// NOTE:
+	// Here's where I'm building out some HTML, so I've broken the following code into sections to make it less of a
+	// mess to read
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// QUESTION
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	// Get the area of the form we are working with
 	var questions = document.getElementById("questions");
@@ -34,13 +40,26 @@ document.getElementById("addQuestion").addEventListener("click", function() {
 	questionLabel.for = 'question' + (lastQuestion + 1);
 	questionLabel.innerText = 'Question #' + (lastQuestion + 1) + ':';
 
+	// Create a line break
+	var br = document.createElement('br');
 
-	var newQuestion = document.createElement('input');
-	newQuestion.id = 'question' + (lastQuestion + 1);
-	newQuestion.name = 'question' + (lastQuestion + 1);
-	newQuestion.type = 'text';
-	newQuestion.className = 'question';
+		// Append the line break onto the question label
+		questionLabel.appendChild(br);
 
+	// Create a new input for the question
+	var questionInput = document.createElement('input');
+	questionInput.id = 'question' + (lastQuestion + 1);
+	questionInput.name = 'question' + (lastQuestion + 1);
+	questionInput.type = 'text';
+	questionInput.className = 'question';
+
+		// Append the input onto the question label
+		questionLabel.appendChild(questionInput);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ANSWER
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	// Create a new answer input box, associated attributes, and button to append
 	var newAnswer = document.createElement('ul');
@@ -48,53 +67,54 @@ document.getElementById("addQuestion").addEventListener("click", function() {
 	// Create the answer <li>
 	var li = document.createElement('li');
 
+		// Attach the <li> to the <ul>
+		newAnswer.appendChild(li);
+
 	// Create the answer label
 	var label = document.createElement('label');
 	label.innerText = 'Q' + (lastQuestion + 1) + ' Answer #1:';
+
+		// Attach the <label> to the <li>
+		li.appendChild(label);
+
+	// Create a line break
+	var br = document.createElement('br');
+
+		// Attach the line break to the <li>
+		li.appendChild(br);
 
 	// Create the answer input box
 	var input = document.createElement('input');
 	input.type = 'text';
 	input.class = 'answer';
 
-	// Create a new clearfix
-	var clearfix = document.createElement('div');
-	clearfix.className = 'clearfix';
+		// Attach the input to the <li>
+		li.appendChild(input);
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ATTACH EVERYTHING TO FORM
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	// Create a few last elements
 	var hr = document.createElement('hr');
 	var br = document.createElement('br');
 
-	// Attach these to the <li>
-	li.appendChild(label);
-	li.appendChild(br);
-	li.appendChild(input);
-
-	// Attach the <li> to the <ul>
-	newAnswer.appendChild(li);
-
+	// Create a new clearfix
+	var clearfix = document.createElement('div');
+	clearfix.className = 'clearfix';
 
 
 	// Attach it all to the end of the form
 	questions.appendChild(hr);
+
 	questions.appendChild(questionLabel);
-	var br = document.createElement('br');
-	questions.appendChild(br);
-	questions.appendChild(newQuestion);
 	questions.appendChild(newAnswer);
-	var br = document.createElement('br');
+
 	questions.appendChild(br);
+
 	questions.appendChild(document.getElementById("addQuestion"));
 	questions.appendChild(clearfix);
 
 });
-
-
-function addQuestion() {
-	console.log('adding question');
-}
-
-function addChoice() {
-	write.innerHTML += 'Add answer: <input type="text" id="answer"' + answers + '/> <br />';
-	answers++;
-}
